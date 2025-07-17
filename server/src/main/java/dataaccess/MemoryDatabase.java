a1f45c533d89a8f815c49a4970e43fedaffd3545
bababundy <koltbundy@gmail.com>
2025-07-16 21:24:08 -0600

    new memory database refactored to work with hashmap

---
+++ server/src/main/java/dataaccess/MemoryDatabase.java
@@ -0,0 +1,17 @@
+package dataaccess;
+
+import model.*;
+
+import java.util.*;
+
+public class MemoryDatabase {
+    public final Map<String, UserData> users = new HashMap<>(); //String = username
+    public final Map<String, AuthData> authUsers = new HashMap<>(); // key = authToken
+    public final Map<Integer, GameData> games = new HashMap<>();// Integer = gameID
+
+    private static final MemoryDatabase instance = new MemoryDatabase();
+
+    public static MemoryDatabase getInstance() {
+        return instance;
+    }
+}
