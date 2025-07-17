1c68457a8d69d53f7312a017eaaf220d81846595
bababundy <koltbundy@gmail.com>
2025-07-16 21:25:46 -0600

    ClearService working with unit tests

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,12 +1,18 @@
 package service;
 
+import dataaccess.*;
 import results.ClearResult;
 
 public class ClearService {
+    private static final UserDao userDAO = new UserDao();
+    private static final AuthDao authDAO = new AuthDao();
+    private static final GameDao gameDAO = new GameDao();
 
     public static ClearResult clear() {
-        //1. verify input
-        //2. database operation
+        //1. database operations (clear users, clear games, clear authtokens)
+        authDAO.clear();
+        gameDAO.clear();
+        userDAO.clear();
         //3. create result and return
         return new ClearResult(null);
     }
