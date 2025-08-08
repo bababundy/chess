2772647b512cf3519dd7f507afea84f1cc3803d3
bababundy <koltbundy@gmail.com>
2025-08-07 21:39:24 -0600

    added equals and hashcode in connection

--- server/src/main/java/server/websocket/Connection.java
+++ server/src/main/java/server/websocket/Connection.java
@@ -3,6 +3,7 @@ package server.websocket;
 import org.eclipse.jetty.websocket.api.Session;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Connection {
     public String authToken;
@@ -17,5 +18,19 @@ public class Connection {
 
     public void send(String msg) throws IOException {
         session.getRemote().sendString(msg);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Connection that = (Connection) o;
+        return gameID == that.gameID && Objects.equals(authToken, that.authToken) && Objects.equals(session, that.session);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(authToken, gameID, session);
     }
 }
